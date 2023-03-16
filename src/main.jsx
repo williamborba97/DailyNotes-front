c68178f509bf49ba839568435cca566b4900c32a
william <williamborba97@gmail.com>
2023-03-16 18:09:59 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {ThemeProvider} from "styled-components";
+
+import theme from './styles/theme'
+import GlobalStyles from './styles/global';
+
+import { Details } from './pages/details';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <Details />
+
+    </ThemeProvider>
+    </React.StrictMode>,
+);
